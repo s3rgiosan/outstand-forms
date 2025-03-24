bf6aed3f6e93c7b55f355e0d35493c3faf4254dc
Sérgio Santos <me@s3rgiosan.com>
2025-03-24 18:27:52 +0000

    Rename component

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -5,7 +5,7 @@ import FieldInput from '../FieldInput';
 import FieldLabel from '../FieldLabel';
 import FieldDescription from '../FieldDescription';
 
-export default function FormField({ type = 'text', attributes, setAttributes }) {
+export default function Field({ type = 'text', attributes, setAttributes }) {
 	const { labelPosition, descriptionPosition } = attributes;
 
 	const hasInlineLabel = labelPosition === 'left' || labelPosition === 'right';
@@ -14,25 +14,27 @@ export default function FormField({ type = 'text', attributes, setAttributes }) 
 	const descriptionField = (
 		<FieldDescription attributes={attributes} setAttributes={setAttributes} />
 	);
-	const inputField = <FieldInput type={type} {...attributes} setAttributes={setAttributes} />;
+	const inputField = (
+		<FieldInput type={type} attributes={attributes} setAttributes={setAttributes} />
+	);
 
 	return (
 		<>
-			{labelPosition === 'top' && labelField}
-			{descriptionPosition === 'top' && descriptionField}
+			{labelPosition !== 'right' && labelField}
+			{!hasInlineLabel && descriptionPosition === 'top' && descriptionField}
 
 			{hasInlineLabel ? (
 				<div className="osf__field-wrapper">
-					{labelPosition === 'left' && labelField}
+					{descriptionPosition === 'top' && descriptionField}
 					{inputField}
-					{labelPosition === 'right' && labelField}
+					{descriptionPosition === 'bottom' && descriptionField}
 				</div>
 			) : (
 				inputField
 			)}
 
-			{descriptionPosition === 'bottom' && descriptionField}
-			{labelPosition === 'bottom' && labelField}
+			{!hasInlineLabel && descriptionPosition === 'bottom' && descriptionField}
+			{labelPosition === 'right' && labelField}
 		</>
 	);
 }
