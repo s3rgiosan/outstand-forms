e378a7293dee90f6d112ba42e2a36a7dbf18bd40
Sérgio Santos <me@s3rgiosan.com>
2026-02-03 22:39:27 +0000

    Update icons

--- src/blocks/field-textarea/icon.js
+++ src/blocks/field-textarea/icon.js
@@ -7,18 +7,18 @@ export const icon = () => {
 	return (
 		<SVG
 			xmlns="http://www.w3.org/2000/svg"
-			width="16"
-			height="16"
+			width="24"
+			height="24"
 			fill="none"
 			stroke="currentColor"
 			strokeLinecap="round"
 			strokeLinejoin="round"
 			strokeWidth="1.5"
-			viewBox="0 0 24 24"
 			className="osf__icon"
+			viewBox="0 0 24 24"
 		>
-			<Path d="M3 6h18M3 12h15a3 3 0 1 1 0 6h-4" />
-			<Path d="m16 16-2 2 2 2M3 18h7" />
+			<Path stroke="none" d="M0 0h24v24H0z"></Path>
+			<Path d="M4 6h16M4 18h5M4 12h13a3 3 0 0 1 0 6h-4l2-2m0 4-2-2"></Path>
 		</SVG>
 	);
 };
--- src/blocks/form/icon.js
+++ src/blocks/form/icon.js
@@ -7,18 +7,19 @@ export const icon = () => {
 	return (
 		<SVG
 			xmlns="http://www.w3.org/2000/svg"
-			width="16"
-			height="16"
+			width="24"
+			height="24"
 			fill="none"
 			stroke="currentColor"
 			strokeLinecap="round"
 			strokeLinejoin="round"
 			strokeWidth="1.5"
-			viewBox="0 0 24 24"
 			className="osf__icon"
+			viewBox="0 0 24 24"
 		>
-			<Rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
-			<Path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2M12 11h4M12 16h4M8 11h.01M8 16h.01" />
+			<Path stroke="none" d="M0 0h24v24H0z"></Path>
+			<Path d="M9 5H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2"></Path>
+			<Path d="M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2M9 12h.01M13 12h2M9 16h.01M13 16h2"></Path>
 		</SVG>
 	);
 };
@@ -34,10 +35,12 @@ export const blank = () => {
 			strokeLinecap="round"
 			strokeLinejoin="round"
 			strokeWidth="1.5"
-			viewBox="0 0 24 24"
 			className="osf__icon"
+			viewBox="0 0 24 24"
 		>
-			<Path d="M10 17H7l-4 4v-7M14 17h1M14 3h1M19 3a2 2 0 0 1 2 2M21 14v1a2 2 0 0 1-2 2M21 9v1M3 9v1M5 3a2 2 0 0 0-2 2M9 3h1" />
+			<Path stroke="none" d="M0 0h24v24H0z"></Path>
+			<Path d="M9 5H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2"></Path>
+			<Path d="M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2"></Path>
 		</SVG>
 	);
 };
